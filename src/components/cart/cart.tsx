0c7893f5b69cb4d45a3aad11b73632a9b58bb413
freund-moberg <jakov.freund@moberg.hr>
2022-11-30 15:26:40 +0100

    menu and cart floating, cart grid formatting

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -1,18 +1,54 @@
 import "./cart.scss";
-import CartItemComponent from "./cartItemComponent";
+import { useDispatch } from "react-redux";
+import { addToCart, removeFromCart } from "../../redux/cartReducer";
+import IconButton from "../UI/iconButton";
 import { useSelector } from "react-redux";
 import { RootState } from "../..";
+import { CartItem } from "../../common/types";
 
 const Cart = () => {
+    const dispatch = useDispatch();
     const cartItemList = useSelector((state: RootState) => state.cart.value.items);
+
+    const plusClickHandler = (itemName: string) => {
+        dispatch(addToCart({ name: itemName, amount: 1 }));
+    };
+
+    const minusClickHandler = (itemName: string) => {
+        dispatch(removeFromCart({ name: itemName, amount: 1 }));
+    };
+    
+    const calculateTotalPrice = (items: CartItem[]) => {
+        return items.reduce((a, b) => a + b.amount * 25, 0); //price
+    };
 
     return (
         <div className="cart">
             <h3>Cart</h3>
             {cartItemList.map((cartItem) => {
-                return <CartItemComponent key={cartItem.name} name={cartItem.name} amount={cartItem.amount} />;
+                return (
+                    <div className="cart-item">
+                        <p>{cartItem.amount}x</p>
+                        <IconButton
+                            imageFilename="button_icons/minus_button.png"
+                            className="cart-item__button minus_button"
+                            onClick={() => minusClickHandler(cartItem.name)}
+                        />
+                        <p>{cartItem.name}</p>
+                        <IconButton
+                            imageFilename="button_icons/plus_button.png"
+                            className="cart-item__button plus_button"
+                            onClick={() => plusClickHandler(cartItem.name)}
+                        />
+                        <p>{cartItem.amount ? 25 * cartItem.amount : "?"}€</p>
+                    </div>
+                );
             })}
-            <p className="cart-total">Total Price:</p>
+            <div className="cart-total">
+                <h4>Total Price:</h4>
+                <div></div>
+                <h4>{calculateTotalPrice(cartItemList)}€</h4>
+            </div>
         </div>
     );
 };
--- src/components/store/store.tsx
+++ src/components/store/store.tsx
@@ -4,6 +4,7 @@ import StoreItems from "./storeItems";
 const Store = () => {
     return (
         <div className="store">
+            <br />
             <p>Store</p>
             <Categories />
             <StoreItems />

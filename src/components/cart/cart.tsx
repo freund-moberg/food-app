b6c4cfdcd6eca2635275b80d58d109a2ba10b64c
freund-moberg <jakov.freund@moberg.hr>
2022-12-07 14:22:02 +0100

    cart buy button

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -4,10 +4,12 @@ import { addToCart, removeFromCart } from "redux/reducers/cartReducer";
 import IconButton from "../UI/iconButton";
 import { useSelector } from "react-redux";
 import { RootState } from "redux/store";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
     const dispatch = useDispatch();
     const cartItemList = useSelector((state: RootState) => state.cart.items);
+    const loggedIn = useSelector((state: RootState) => state.account.loggedIn);
 
     const plusClickHandler = (itemName: string) => {
         dispatch(addToCart({ name: itemName, amount: 1 }));
@@ -25,32 +27,52 @@ const Cart = () => {
         <div className="cart">
             <h3>Cart</h3>
             <div className="cart-items">
-            {cartItemList.map((cartItem) => {
-                return (
-                    <div className="cart-item" key={cartItem.name}>
-                        <p>{cartItem.amount}x</p>
-                        <IconButton
-                            imageFilename="button_icons/minus_button.png"
-                            className="cart-item__button minus_button"
-                            onClick={() => minusClickHandler(cartItem.name)}
-                        />
-                        <p>{cartItem.name}</p>
-                        <IconButton
-                            imageFilename="button_icons/plus_button.png"
-                            className="cart-item__button plus_button"
-                            onClick={() => plusClickHandler(cartItem.name)}
-                        />
-                        <p>{cartItem.amount ? cartItem.price * cartItem.amount : "?"}€</p>
-                    </div>
-                );
-            })}
+                {cartItemList.map((cartItem) => {
+                    return (
+                        <div className="cart-item" key={cartItem.name}>
+                            <p>{cartItem.amount}x</p>
+                            <IconButton
+                                imageFilename="button_icons/minus_button.png"
+                                className="cart-item__button minus_button"
+                                onClick={() => minusClickHandler(cartItem.name)}
+                            />
+                            <p>{cartItem.name}</p>
+                            <IconButton
+                                imageFilename="button_icons/plus_button.png"
+                                className="cart-item__button plus_button"
+                                onClick={() => plusClickHandler(cartItem.name)}
+                            />
+                            <p>
+                                {cartItem.amount ? cartItem.price * cartItem.amount : "?"}
+                                €
+                            </p>
+                        </div>
+                    );
+                })}
             </div>
             <div className="cart-total">
+                {cartItemList.length > 0 ? (
+                    loggedIn ? (
+                        <IconButton
+                            imageFilename="button_icons/cart_button.png"
+                            className="cart-total__buy"
+                        />
+                    ) : (
+                        <Link to="/profile/login">
+                            <IconButton
+                                imageFilename="button_icons/logout_button.png"
+                                className="cart-total__login"
+                            />
+                        </Link>
+                    )
+                ) : (
+                    <div />
+                )}
+                {}
                 <h4>Total Price:</h4>
                 <div></div>
                 <h4>{calculateTotalPrice()}€</h4>
             </div>
-            <div>TODO Login to buy</div>
         </div>
     );
 };
